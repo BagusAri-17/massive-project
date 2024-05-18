88e81c4ad54ae8d0bc6298b5df326a99b7b6690b
BagusAri-17 <bagusari.widhi@gmail.com>
2024-05-18 21:18:18 +0800

    initial project

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "16px",
+          sm: "16px",
+          md: "32px",
+          lg: "64px",
+          xl: "86px",
+        },
+      },
+
+      screens: {
+        lg: "1280px",
+        xl: "1440px"
+      },
+    },
+  },
+  plugins: [],
+}
